bcbfeb1f7e603b44f66f77722d55433febd35966
chhengle <leangc1405@gmail.com>
2023-11-18 14:58:09 +0700

    add class staff

---
+++ DB/StaffDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cafe_pos_system.Models;
+
+namespace cafe_pos_system.DB
+{
+    public class StaffDB
+    {
+        private SqlConnection con = POSCafeDB.GetConnection();
+    }
+   public Staff GetStaffByStaffID(int ID)
+    {
+         throw new NotImplementedException();
+    }
+
+}
